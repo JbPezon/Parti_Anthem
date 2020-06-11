f90b87af4457d170edd06830aad9af9a320cea89
BRADU Gauthier <gauthier.bradu@isen.yncrea.fr>
2020-06-11 16:22:51 +0200

    creation de get et set pour neurone et layer

--- src/layer.java
+++ src/layer.java
@@ -9,4 +9,20 @@ public class layer {
         this.nombreneurone=nbneurone;
         this.myNeurone= new ArrayList<neurone>();
     }
+
+    public int getNombreneurone() {
+        return nombreneurone;
+    }
+
+    public void setNombreneurone(int nombreneurone) {
+        this.nombreneurone = nombreneurone;
+    }
+
+    public ArrayList<neurone> getMyNeurone() {
+        return myNeurone;
+    }
+
+    public void setMyNeurone(ArrayList<neurone> myNeurone) {
+        this.myNeurone = myNeurone;
+    }
 }
--- src/neurone.java
+++ src/neurone.java
@@ -12,4 +12,36 @@ public class neurone {
         this.conectionEntre= new ArrayList<connection>();
         this.conectionSortie= new ArrayList<connection>();
     }
+
+    public float getBias() {
+        return bias;
+    }
+
+    public void setBias(float bias) {
+        this.bias = bias;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ArrayList<connection> getConectionEntre() {
+        return conectionEntre;
+    }
+
+    public void setConectionEntre(ArrayList<connection> conectionEntre) {
+        this.conectionEntre = conectionEntre;
+    }
+
+    public ArrayList<connection> getConectionSortie() {
+        return conectionSortie;
+    }
+
+    public void setConectionSortie(ArrayList<connection> conectionSortie) {
+        this.conectionSortie = conectionSortie;
+    }
 }
