32c1545b4955d93215196a32c2214850b5ef1631
JbPezon <jb.pezonchanut@gmail.com>
2020-06-16 16:21:28 +0200

    Tentative de fonction backpropagation

--- src/neurone.java
+++ src/neurone.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class neurone {
     private float bias;
-    private int data;
+    private float data;
     private ArrayList<connection> conectionEntre;
     private ArrayList<connection> conectionSortie;
 
@@ -11,6 +11,47 @@ public class neurone {
         this.data=0;
         this.conectionEntre= new ArrayList<connection>();
         this.conectionSortie= new ArrayList<connection>();
+    }
+
+    public void backPropagation(boolean bonneNeurone){
+        float nouveauPoid = 0;
+        if (bonneNeurone){
+            this.setBias(this.getBias() - (this.getData()*(this.getData()-1)));
+            for (connection c: this.conectionEntre) {
+                nouveauPoid = c.getPoid() - ((this.getData()*(this.getData() - 1))*(float)0.01);
+                c.setPoid(nouveauPoid);
+            }
+        }else{
+            this.setBias(this.getBias() - (this.getData()*this.getData()));
+            for (connection c: this.conectionEntre) {
+                nouveauPoid = c.getPoid() - ((this.getData() * this.getData()) * (float)0.01);
+                c.setPoid(nouveauPoid);
+            }
+        }
+
+
+
+        /*
+        float n = 0.05f;
+        dwi[i] = f[i] * (1 - f[i]) * vWeights[i] * dv;
+        f = liste des data en sortie
+
+        for (int i = 0;i<neurons;i++){
+            dwi[i] = f[i] * (1 - f[i]) * vWeights[i] * dv;
+            for (int j = 0;j<dimension;j++){
+                dw[j][i] = n * dwi[i] * x[j];
+                this.wWeights[j][i] = wWeights[j][i] + dw[j][i];
+            }
+        }
+
+        // Modify bias
+        for (int i = 0;i<neurons;i++){
+            dbi[i] = f[i] * (1 - f[i]) * vWeights[i] * dv;
+            db[i] = n * dbi[i] * 1;
+            this.bias[i] = bias[i] + db[i];
+        }
+        */
+
     }
 
     public void addConnectionEntre(connection co){
@@ -29,7 +70,7 @@ public class neurone {
         this.bias = bias;
     }
 
-    public int getData() {
+    public float getData() {
         return data;
     }
 
